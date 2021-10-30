917612e0c4289e0c0674ab8b6d2ab52fa8ab27ec
AdrianX <adrinium@gmail.com>
2021-10-30 14:08:16 +0300

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import './homepage.styles.scss';
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+
+            </div>
+        </div>
+    )
+}
